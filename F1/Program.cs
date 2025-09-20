984c231ea2e411e166113280c600aa3f8c0028f3
Lenny <mubealenny@gmail.com>
2025-09-20 17:47:51 +0300

    feat(api): setup minimal API with Swagger and sample endpoints
    - Added Swagger/OpenAPI support with Swashbuckle
    - Configured endpoints for /age and /temperature
    - Enabled Swagger UI for API exploration

---
+++ F1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Lenny API", Version = "v1"});
+});
+
+var app = builder.Build();
+
+
+//Enable swagger
+app.UseSwagger();
+app.UseSwaggerUI();    
+
+
+app.MapGet("/device_Condition", () => "ac_1: running " + "ac_2: failed");
+
+//ambient condition
+app.MapGet("/temperature", () => 
+{
+    var temp = new
+    {
+        Date = DateTime.Now.ToString("yyyy-MM-dd"),
+        Temp = 25,
+        Summary = "moderate"
+    };
+
+    return temp;
+
+})
+.WithName("GetTemp")
+.WithOpenApi();
+
+app.Run(); 
