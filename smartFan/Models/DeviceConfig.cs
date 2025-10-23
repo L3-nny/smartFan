764ea11eef70e1669f6ef7b6c00c6477b0f2f09a
L3-nny <mubealenny@gmail.com>
2025-10-23 22:41:05 +0300

    Added class entity for the device config attribute

---
+++ smartFan/Models/DeviceConfig.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace smartFan.Models
+{
+    public class DeviceConfig
+    {
+        public int Id { get; set; }
+        public int DeviceId { get; set; }
+        public double MinTemp { get; set; }
+        public double MaxTemp { get; set; }
+        public double FluctuationStep { get; set; }
+        public bool AutoModeEnabled { get; set; }
+
+        //Navigation properties
+        public ICollection<TemperatureLog> TemperatureLogs { get; set; }
+        public ManualOverride ManualOverride { get; set; }
+        public ICollection<ErrorLog> ErrorLogs { get; set; }
+
+    }
+}
