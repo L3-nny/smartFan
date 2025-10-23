6f556fab1a0c5d4af8e3db276c353c1eb72c586f
L3-nny <mubealenny@gmail.com>
2025-10-23 22:24:56 +0300

    Included class entity for the manual override feature

---
+++ smartFan/Models/ManualOverride.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace smartFan.Models
+{
+    public class ManualOverride
+    {
+        public int Id { get; set; }
+        public int DeiceConfigId { get; set; }
+        public string FanSpeed { get; set; }
+        public DateTime OverrideUntil { get; set; }
+        public bool Active { get; set; }
+
+        //Navigation
+        public DeviceConfig DeviceConfig { get; set; }
+    }
+}
