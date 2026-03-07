3cba65e2090df14ec0b54c61a91271d676f85087
L3-nny <mubealenny@gmail.com>
2026-03-07 18:08:17 +0300

    feat(math): add ThermalMath class with Steinhart-Hart equation constants

---
+++ smartFan.Embedded/include/thermal_math.h
@@ -0,0 +1,15 @@
+#ifndef THERMAL_MATH_H
+#define THERMAL_MATH_H
+
+#include <math.h>
+
+class ThermalMath {
+public:
+// Constants for the Steinhart-Hart equation
+static constexpr double A = 0.001129148;
+static constexpr double B = 0.000234125;
+static constexpr double C = 0.0000000876741;
+
+};
+
+#endif
