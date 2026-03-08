c1697e2d3f659fc65ae1ff95262767e3eed52b28
L3-nny <mubealenny@gmail.com>
2026-03-08 12:40:16 +0300

    feat(gateway): add GatewayClient class for managing server communication and telemetry

---
+++ smartFan.Embedded/include/gateway_client.h
@@ -0,0 +1,26 @@
+#ifndef GATEWAY_CLIENT_H
+#define GATEWAY_CLIENT_H
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+class GatewayClient {
+public:
+    // Structure to hold the server's response
+    struct FanCommand {
+        int speed;
+        int mode;
+        bool success;
+    };
+
+    void init();
+    void maintainConnection();
+    FanCommand sendTelemetry(float temperature);
+
+private:
+    unsigned long lastWifiRetry = 0;
+};
+
+#endif
