f67578ca726d5cb426367af5c030229992b94515
L3-nny <mubealenny@gmail.com>
2026-03-08 16:58:53 +0300

    Include fallback speed in case of server error

--- smartFan.Embedded/src/fan_controller.cpp
+++ smartFan.Embedded/src/fan_controller.cpp
@@ -6,12 +6,27 @@ void FanController::init() {
     ledcAttachPin(MOSFET_PIN, 0);
 }
 
-void FanController::setSpeed(int level) {
+void FanController::updateState(int speed, int mode) {
+
     int pwmValue = 0;
-    switch (level) {
+    switch (speed) {
         case 1: pwmValue = 85; break; 
         case 2: pwmValue = 170; break;
         case 3: pwmValue = 255; break; 
         default: pwmValue = 0; break; 
     }
-    ledcWrite(0, pwmValue);}
+
+    ledcWrite(0, pwmValue);
+
+    //Visual feedback for manual mode
+    if (mode == 1) {
+        digitalWrite(STATUS_LED_PIN, !digitalRead(STATUS_LED_PIN)); 
+    } else {
+        digitalWrite(STATUS_LED_PIN, HIGH);
+    }
+}
+
+void FanController::setEmergencySpeed() {
+    ledcWrite(0, 85);
+    digitalWrite(STATUS_LED_PIN, LOW); //Off to indicate error
+}
