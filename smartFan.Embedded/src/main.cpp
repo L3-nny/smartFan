cfdf9ad765871a5222c79cde371be2d3a41b3fa1
L3-nny <mubealenny@gmail.com>
2026-03-09 23:29:49 +0300

    Included debug statements

--- smartFan.Embedded/src/main.cpp
+++ smartFan.Embedded/src/main.cpp
@@ -7,6 +7,11 @@ SystemManager fanSystem;
 void setup() {
     Serial.begin(115200);
     fanSystem.setup();
+
+    // Debug output to confirm WiFi connection and IP address
+    //REMOVE IN PROD; MAYBE
+    Serial.print("\n[Network] ESP32 IP: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop() {
