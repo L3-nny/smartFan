71996d9c46aca98bf10a0120cf9f236681958766
L3-nny <mubealenny@gmail.com>
2026-03-24 17:02:11 +0300

    feat: implement MqttBackgroundService for real-time telemetry

---
+++ smartFan/Services/Interfaces/MqttBackgroundService.cs
@@ -0,0 +1,62 @@
+using MQTTnet;
+using MQTTnet.Extensions.ManagedClient;
+using MQTTnet.Packets;
+using smartFan.Models.DTOs;
+using smartFan.Services.Interfaces;
+using System.Text;
+using System.Text.Json;
+
+public class MqttBackgroundService : BackgroundService
+{
+    private readonly IManagedMqttClient _mqttClient;
+    private readonly ISensorService _sensorService;
+    private readonly ILogger<MqttBackgroundService> _logger;
+
+    public MqttBackgroundService(ISensorService sensorService, ILogger<MqttBackgroundService> logger)
+    {
+        _sensorService = sensorService;
+        _logger = logger;
+        _mqttClient = new MqttFactory().CreateManagedMqttClient();
+
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var mqttOptions = new ManagedMqttClientOptionsBuilder()
+            .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+            .WithClientOptions(new MqttClientOptionsBuilder()
+                .WithClientId("SmartFanSensorClient")
+                .WithTcpServer("localhost", 1883)
+                .WithCredentials("", "")
+                .Build())
+            .Build();
+
+        _mqttClient.ConnectedAsync += e =>
+        {
+            _logger.LogInformation("Connected to MQTT broker.");
+            return Task.CompletedTask;
+        };
+
+
+        _mqttClient.ApplicationMessageReceivedAsync += async e =>
+        {
+            try
+            {
+                var payload = Encoding.UTF8.GetString(e.ApplicationMessage.PayloadSegment);
+                _logger.LogInformation($"Received MQTT message on {e.ApplicationMessage.Topic}: {payload}");
+
+                var data = JsonSerializer.Deserialize<TelemetryModel>(payload, new JsonSerializerOptions);
+                _sensorService.RegisterHardwareReading(data);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error occurred while processing MQTT message.");
+            }
+        };
+
+        await _mqttClient.SubscribeAsync("smartFan/telemetry");
+        await _mqttClient.StartAsync(mqttOptions);
+
+        await Task.Delay(-1, stoppingToken);
+    }
+}
