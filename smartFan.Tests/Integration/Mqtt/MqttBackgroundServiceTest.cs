b010f0859cb44c5d193dd20407700e1cb38a49c9
L3-nny <mubealenny@gmail.com>
2026-03-25 21:00:47 +0300

    Include relevant classes

---
+++ smartFan.Tests/Integration/Mqtt/MqttBackgroundServiceTest.cs
@@ -0,0 +1,11 @@
+using Xunit;
+using Moq;
+using MQTTnet;
+using System.Text;
+using System.Text.Json;
+using Microsoft.Extensions.Logging;
+using smartFan.Services;
+using smartFan.Services.Interfaces;
+using smartFan.Models.DTOs;
+using smartFan.Tests.Fixtures;
+
